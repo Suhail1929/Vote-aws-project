3a4a0a3ffb57802cdbf34c6c70f4a05b9a30bc05
suhail1929 <souhailmtarfie@gmail.com>
2024-11-27 16:50:13 +0100

    button candidat

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,9 @@
   <div class="container py-5 text-center">
     <h1>Bienvenue sur le site de l'élection présidentielle 2024</h1>
     <p class="lead">Vous pouvez voter pour votre candidat préféré.</p>
-    <p class="lead">Pour cela, vous devez vous connecter.</p>
+    <p class="lead">Pour cela, vous devez se rendre sur la page des candidats.</p>
+    <p class="lead">Bonne chance à tous les candidats !</p>
+    <RouterLink to="/candidat" class="btn btn-primary mt-3">Voir les candidats</RouterLink>
   </div>
 </template>
   
